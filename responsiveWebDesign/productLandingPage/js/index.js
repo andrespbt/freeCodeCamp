c3d70097bfbf792d223e2ed960b0c1e57baef218
andrespbt <andrespbt14@hotmail.com>
2022-08-14 03:07:15 -0300

    Uploading product landing page finished

---
+++ responsiveWebDesign/productLandingPage/js/index.js
@@ -0,0 +1,28 @@
+window.onscroll = function () {
+  stickyFunction();
+};
+
+const navBar = document.querySelector("#nav-bar");
+const header = document.querySelector("#header");
+const hamburger = document.querySelector(".hamburger");
+const ulNavContainer = document.querySelector(".nav-list--container");
+const sticky = header.offsetHeight;
+
+function stickyFunction() {
+  if (window.scrollY >= sticky) {
+    header.classList.add("sticky");
+    navBar.classList.add("navSticky");
+    hamburger.classList.add("hamburger--sticky");
+    document.body.classList.add("content");
+  } else {
+    header.classList.remove("sticky");
+    navBar.classList.remove("navSticky");
+    hamburger.classList.remove("hamburger--sticky");
+    document.body.classList.remove("content");
+  }
+}
+
+hamburger.addEventListener("click", () => {
+  ulNavContainer.classList.toggle("nav-list--container--mobile");
+  ulNavContainer.classList.toggle("nav-list--container");
+});
