68c5bd915b7af0d8b42c067af94b57a87d971dc3
andrespbt <andrespbt14@hotmail.com>
2022-08-21 20:05:05 -0300

    Small changes landing page

--- responsiveWebDesign/productLandingPage/js/index.js
+++ responsiveWebDesign/productLandingPage/js/index.js
@@ -1,28 +1,30 @@
-window.onscroll = function () {
-  stickyFunction();
-};
+window.onload = function () {
+  window.onscroll = function () {
+    stickyFunction();
+  };
 
-const navBar = document.querySelector("#nav-bar");
-const header = document.querySelector("#header");
-const hamburger = document.querySelector(".hamburger");
-const ulNavContainer = document.querySelector(".nav-list--container");
-const sticky = header.offsetHeight;
+  const navBar = document.querySelector("#nav-bar");
+  const header = document.querySelector("#header");
+  const hamburger = document.querySelector(".hamburger");
+  const ulNavContainer = document.querySelector(".nav-list--container");
+  const sticky = header.offsetHeight;
 
-function stickyFunction() {
-  if (window.scrollY >= sticky) {
-    header.classList.add("sticky");
-    navBar.classList.add("navSticky");
-    hamburger.classList.add("hamburger--sticky");
-    document.body.classList.add("content");
-  } else {
-    header.classList.remove("sticky");
-    navBar.classList.remove("navSticky");
-    hamburger.classList.remove("hamburger--sticky");
-    document.body.classList.remove("content");
+  function stickyFunction() {
+    if (window.scrollY >= sticky) {
+      header.classList.add("sticky");
+      navBar.classList.add("navSticky");
+      hamburger.classList.add("hamburger--sticky");
+      document.body.classList.add("content");
+    } else {
+      header.classList.remove("sticky");
+      navBar.classList.remove("navSticky");
+      hamburger.classList.remove("hamburger--sticky");
+      document.body.classList.remove("content");
+    }
   }
-}
 
-hamburger.addEventListener("click", () => {
-  ulNavContainer.classList.toggle("nav-list--container--mobile");
-  ulNavContainer.classList.toggle("nav-list--container");
-});
+  hamburger.addEventListener("click", () => {
+    ulNavContainer.classList.toggle("nav-list--container--mobile");
+    ulNavContainer.classList.toggle("nav-list--container");
+  });
+};
